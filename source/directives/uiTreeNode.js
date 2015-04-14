099bb9563f4435e9742b16fbf00c64b99d55b0ba
samuv <verzi@nweb.it>
2015-04-14 11:31:32 +0200

    fix dragStart event

--- source/directives/uiTreeNode.js
+++ source/directives/uiTreeNode.js
@@ -146,13 +146,14 @@
 
             document_height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
             document_width = Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth);
-          };
-
-          var dragMove = function(e) {
 
             scope.$apply(function() {
               scope.$callbacks.dragStart(dragInfo.eventArgs(elements, pos));
             });
+
+          };
+
+          var dragMove = function(e) {
 
             var eventObj = $uiTreeHelper.eventObj(e);
             var prev, leftElmPos, originLeftElmPos, topElmPos, originTopElmPos;
@@ -182,7 +183,7 @@
                 scrollTopBoundTo = $(scope.boundTo).scrollTop();
                 if (scrollTopBoundTo > 100) {
                   $(scope.boundTo).animate({
-                    scrollTop: '-=70'
+                    scrollTop: '-=35'
                   }, 100);
                 }
               }
@@ -201,7 +202,7 @@
                 scrollTopBoundTo = $(scope.boundTo).scrollTop();
 
                 $(scope.boundTo).animate({
-                  scrollTop: '+=70'
+                  scrollTop: '+=35'
                 }, 100);
               }
 
